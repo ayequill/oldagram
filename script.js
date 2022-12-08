6f0534a89f82c6506e51c7cb9741a67bbca74a8c
ayequill <ayequillgh@gmail.com>
2022-12-08 08:50:38 -0600

    used js for layout

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const posts = [
     avatar: "/img/Vincent van Gogh.png",
     post: "/img/post-image-one.png",
     comment: "just took a few mushrooms lol",
-    likes: 21492,
+    likes: 21,
   },
   {
     name: "Gustave Courbet",
     username: "gus1819",
     location: "Ornans, France",
     avatar: "img/avatar-courbet.png",
-    post: "images/post-courbet.jpg",
+    post: "/img/post-image-two.png",
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
   },
@@ -21,71 +21,13 @@ const posts = [
     name: "Joseph Ducreux",
     username: "jd1735",
     location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
+    avatar: "img/Joseph Ducreux.png",
+    post: "/img/post-image-three.png",
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
   },
 ];
-//  declaring DOM variables
-const likeIcon = document.createElement("img");
-likeIcon.setAttribute("class", "icon");
-likeIcon.setAttribute("src", "/img/like.png");
-
-const shareIcon = document.createElement("img");
-shareIcon.setAttribute("class", "icon");
-shareIcon.setAttribute("src", "/img/share.png");
-
-const commentIcon = document.createElement("img");
-setAttributes(commentIcon, { class: "icon", src: "/img/comment.png" });
-
-const header = document.querySelector(".header");
-let section = document.createElement("section");
-
-let container = document.createElement("div");
-container.classList = "container";
-
-let userAvatar = document.createElement("img");
-setAttributes(userAvatar, {
-  class: "avatar",
-  src: "/img/Vincent van Gogh.png",
-});
-
-let name = document.createElement("p");
-setAttributes(name, { class: "name text-color text-sm text-bold" });
-let userLocation = document.createElement("span");
-
-setAttributes(userLocation, { class: "location text-sm" });
-
-let post = document.createElement("img");
-setAttributes(post, { class: "post", src: "/img/post-image-one.png" });
-
-const likesCount = document.createElement("p");
-setAttributes(likesCount, { class: "likes text-sm text-bold" });
-
-const userName = document.createElement("p");
-setAttributes(userName, { class: "username text-color text-sm text-bold" });
-
-const comments = document.createElement("p");
-setAttributes(comments, { class: "comment text-sm text-color" });
-
-// function to render layout
-function renderLayout() {
-  document.body.appendChild(section);
-  // header.nextSibling.after(section, header.nextSibling)
-  section.appendChild(container);
-  container.appendChild(userAvatar);
-  container.appendChild(name);
-  name.append(userLocation);
-  container.appendChild(post);
-  container.appendChild(likeIcon);
-  container.appendChild(commentIcon);
-  container.appendChild(shareIcon);
-  container.appendChild(likesCount);
-  container.appendChild(userName);
-  container.appendChild(comments);
-}
 
 function setAttributes(el, options) {
   Object.keys(options).forEach(function (attr) {
@@ -93,16 +35,68 @@ function setAttributes(el, options) {
   });
 }
 
-function sectionOne() {
-  name.textContent = posts[0].name;
-  likesCount.textContent = `${posts[0].likes} likes`;
-  userName.textContent = "ayequill";
-  comments.textContent = posts[0].comment;
-  // Object.assign(location,layout[3])
+function renderLayout() {
+  posts.forEach(function (posts) {
+    const header = document.querySelector(".header");
+    const section = document.createElement("section");
+    const likeIcon = document.createElement("img");
+    likeIcon.setAttribute("class", "icon");
+    likeIcon.setAttribute("src", "/img/like.png");
 
-  userLocation.textContent = posts[0].location;
+    const shareIcon = document.createElement("img");
+    shareIcon.setAttribute("class", "icon");
+    shareIcon.setAttribute("src", "/img/share.png");
 
-  renderLayout();
+    const commentIcon = document.createElement("img");
+    setAttributes(commentIcon, { class: "icon", src: "/img/comment.png" });
+
+    const container = document.createElement("div");
+    container.classList = "container";
+
+    const userAvatar = document.createElement("img");
+    setAttributes(userAvatar, { class: "avatar", src: posts.avatar });
+
+    const name = document.createElement("p");
+    name.textContent = posts.name;
+    setAttributes(name, { class: "name text-color text-sm text-bold" });
+
+    const userLocation = document.createElement("span");
+    userLocation.textContent = posts.location;
+    setAttributes(userLocation, { class: "location text-sm" });
+
+    const post = document.createElement("img");
+    setAttributes(post, { class: "post", src: posts.post });
+
+    let likesCount = document.createElement("p");
+    likesCount.textContent = `${posts.likes} likes`;
+    setAttributes(likesCount, { class: "likes text-sm text-bold" });
+
+    const userName = document.createElement("p");
+    userName.textContent = posts.username;
+    setAttributes(userName, { class: "username text-color text-sm text-bold" });
+
+    const comments = document.createElement("span");
+    comments.textContent = posts.comment;
+    setAttributes(comments, { class: "comment text-sm text-color" });
+
+    document.body.appendChild(section);
+    section.appendChild(container);
+    container.appendChild(userAvatar);
+    container.appendChild(name);
+    name.append(userLocation);
+    container.appendChild(post);
+    container.appendChild(likeIcon);
+    container.appendChild(commentIcon);
+    container.appendChild(shareIcon);
+    container.appendChild(likesCount);
+    container.appendChild(userName);
+    userName.append(comments);
+
+    post.addEventListener("dblclick", function () {
+      posts.likes += 1;
+      likesCount.textContent = `${posts.likes} likes`;
+    });
+  });
 }
 
-sectionOne();
+renderLayout();
